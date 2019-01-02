b4992043a6faf45dafedd84a136d33090fb1f059
gary <mcsd2001@163.com>
2019-01-02 17:46:34 +0800

    version:0.2

--- hapa.go
+++ hapa.go
@@ -10,14 +10,14 @@ import (
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 
-	html := "This is hapa! <br> A simple HAProxy Agent"
+	html := "<html><head>hapa</head><body><h2>This is hapa! </h2><br> A simple HAProxy Agent<br> <br> <a href=\"bl\">restart</a> <a href=\"logs/raw\">show logs</a> </body></html>"
 	fmt.Fprintf(w, html)
 
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 
-	html := "This is hapa! <br> A simple HAProxy Agent"
+	html := "This is hapa! \n A simple HAProxy Agent"
 	fmt.Fprintf(w, html)
 }
 
@@ -26,7 +26,7 @@ func blacklistHandler(w http.ResponseWriter, r *http.Request) {
 	restartCMD := exec.Command("/bin/sh", "-c", "sudo systemctl restart haproxy.service")
 	statusCMD := exec.Command("/bin/sh", "-c", "sudo systemctl status haproxy.service")
 
-	html := "restarting ...<br>"
+	html := "restarting ...\n"
 	var out bytes.Buffer
 
 	restartCMD.Stdout = &out
@@ -36,7 +36,7 @@ func blacklistHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	html += out.String()
 
-	html += "<br>statusing ...<br>"
+	html += "\nstatusing ...\n"
 	statusCMD.Stdout = &out
 	statusErr := statusCMD.Run()
 	if statusErr != nil {
@@ -47,11 +47,28 @@ func blacklistHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, html)
 }
 
+func logsRawHandler(w http.ResponseWriter, r *http.Request) {
+
+	logsRawCMD := exec.Command("/bin/sh", "-c", "sudo cat /var/log/haproxy-ldap.log")
+
+	var out bytes.Buffer
+
+	logsRawCMD.Stdout = &out
+	logsRawErr := logsRawCMD.Run()
+	if logsRawErr != nil {
+		log.Fatal(logsRawErr)
+	}
+	html := out.String()
+
+	fmt.Fprintf(w, html)
+}
+
 func main() {
 
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/about", aboutHandler)
 	http.HandleFunc("/bl", blacklistHandler)
+	http.HandleFunc("/logs/raw", blacklistHandler)
 	http.ListenAndServe(":9923", nil)
 
 }
